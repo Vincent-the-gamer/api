9f06d4122084a35c42a6f1fb2cfea583e3c8d232
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-02-03 14:47:13 +0800

    chore: lint.

--- server/routes/whoami.get.ts
+++ server/routes/whoami.get.ts
@@ -1,25 +1,26 @@
 /**
  * get local IP of current machine.
  */
-import os from "os"
+import os from 'node:os'
 
 export default eventHandler(() => {
-    // get network interfaces info.
-    const networkInterfaces = os.networkInterfaces();
+  // get network interfaces info.
+  const networkInterfaces = os.networkInterfaces()
 
-    // get first non-loopback IPV4 address.
-    let localIP = '';
-    for (const interfaceName in networkInterfaces) {
-        for (const network of networkInterfaces[interfaceName]) {
-            if (network.family === 'IPv4' && !network.internal) {
-                localIP = network.address;
-                break;
-            }
-        }
-        if (localIP) break;
+  // get first non-loopback IPV4 address.
+  let localIP = ''
+  for (const interfaceName in networkInterfaces) {
+    for (const network of networkInterfaces[interfaceName]) {
+      if (network.family === 'IPv4' && !network.internal) {
+        localIP = network.address
+        break
+      }
     }
+    if (localIP)
+      break
+  }
 
-    return {
-        ip: localIP
-    }
+  return {
+    ip: localIP,
+  }
 })
