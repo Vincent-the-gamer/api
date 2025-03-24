4f693c1eba1837655a54ed1b8b8a300e8f149332
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-03-24 13:18:38 +0800

    chore: debug.

--- server/routes/isPathExists.get.ts
+++ server/routes/isPathExists.get.ts
@@ -4,11 +4,21 @@ import { __dirname } from "~~/tools/files"
 
 export default eventHandler((event) => {
   const { dir }: any = getQuery(event)
+
+  const exists = fs.existsSync(dir)
+  let files = []
+
+  if(exists) {
+    // find files
+    files = fs.readdirSync(dir)
+  }
+
   return {
     code: 200,
     path: dir,
-    pathExists: fs.existsSync(dir),
+    pathExists: exists,
     currentJsPath: __dirname,
     parentPath: path.resolve(dir, "../"),
+    children: files
   }
 })
